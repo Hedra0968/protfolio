fc4a5255342512bde6990520f14e4550d0e688c6
Hedra0968 <hedravictor@gmail.com>
2026-08-27 03:16:40 +0300

    Implement service worker with caching strategy

--- sw.js
+++ sw.js
@@ -1,7 +1,57 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
+const CACHE_NAME = "hedra-portfolio-v1";
+
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./images/me.png",
+  "./images/pro1.png",
+  "./images/pro2.png",
+  "./images/pro3.png",
+  "./images/pro4.png",
+  "./images/pro5.png",
+  "./images/pro6.png"
+];
+
+// Install: cache core assets and activate immediately
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+// Activate: clear old caches and take control of open pages right away
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+// Fetch: network-first, fall back to cache when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
